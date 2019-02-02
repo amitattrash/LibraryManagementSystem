84188d3c43b9d1927fa63a8cb487af1a0428f21d
amitattrash <amitattrash@gmail.com>
2019-02-03 02:28:01 +0530

    Dbscript files changed

--- LibraryManagementSystem/src/com/librarymanagement/dbscripts/dbscripts.sql
+++ LibraryManagementSystem/src/com/librarymanagement/dbscripts/dbscripts.sql
@@ -1,6 +1,10 @@
+--Creation of Database
+create database LibraryManagementSystem;
+
+--Creation of UserCredential Table
 create table UserCredential(name varchar(30),password varchar(30),role varchar(30));
 insert into UserCredential values('Neha','rose','Student');
 insert into UserCredential values('Sikha','lotus','Admin');
 
-
+--Creation of User Table
 create table User(name varchar(20),rollNo varchar(20),mobileNo varchar(20),bloodGroup varchar(5),dob Date,branch varchar(10),session varchar(20),sex varchar(10));
